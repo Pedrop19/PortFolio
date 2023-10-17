e7eb10e0aca14eb53e746314a91fcc45e4717dd2
Pedro Rafael Lázaro Nevado <119618710+Pedrop19@users.noreply.github.com>
2023-10-17 17:25:31 +0200

    Update 0.0.3

--- js/script.js
+++ js/script.js
@@ -2,199 +2,76 @@ document.addEventListener("DOMContentLoaded", function () {
     let textarea = document.getElementById("input_textarea");
     let resultado = document.getElementById("output");
 
-    textarea.addEventListener("keydown", function (event) {
+    fetch('../proyectos.json')
+        .then(response => response.json())
+        .then(data => {
+            const proyectos = data;
 
-
-        if (event.key === "Enter") {
-            let divElement = document.createElement("div");
-            divElement.className = "linea";
-            event.preventDefault();
-            let textoIngresado = textarea.value;
-            if (textoIngresado == "clear" || textoIngresado == "cls") {
-                resultado.textContent = " ";
-            } else if (textoIngresado == "help") {
-                divElement.innerHTML = "<a href='github.com/Pedrop19/'>github.com/Pedrop19/ </a>"
-                    + "<span class='yellow'>" + textoIngresado + "</span><br><br>"
-                    + "<span class='green'>proyectos</span>: <span class='blue'>Muestra una selección de mis proyectos más relevantes.<span><br><br>"
-                    + "<span class='green'>clear o cls</span>: <span class='blue'>Limpia la consola<span>";
-            } else if (textoIngresado == "proyectos") {
-                resultado.textContent = " ";
-                divElement.innerHTML = "<a href='github.com/Pedrop19/'>github.com/Pedrop19/ </a>"
-                    + "<span class='yellow'>" + textoIngresado + "</span><br><br>"
-                    + "<span class='blue'>💻 Proyectos(escribe el comando para acceder al proyecto deseado):</span><br><br>"
-                    + "<span class='green'>codificador</span><br><br>"
-                    + "<span class='green'>cuentaatras</span><br><br>"
-                    + "<span class='green'>millionarios</span><br><br>"
-                    + "<span class='green'>calendario</span><br><br>"
-                    + "<span class='green'>cuadro</span><br><br>"
-                    + "<span class='green'>animacion</span><br><br>"
-            } else if (textoIngresado == "codificador") {
-                resultado.textContent = " ";
-                divElement.innerHTML = "<a href='github.com/Pedrop19/'>github.com/Pedrop19/ </a>"
-                    + "<span class='yellow'>" + textoIngresado + "</span><br><br>"
-                    + "<table class='tabla'>"
-                    + "<tr>"
-                    +  "<th colspan='3'><span class='yellow' id='title'>🎖️ Codificador de Mensajes 🎖️<span></th>"
-                    + "</tr>"
-                    + "<tr>"
-                    +  "<td rowspan='3'><img src='./img/codifi.PNG' width='300px'></td>"
-                    +  "<td colspan='3' align='center'><h3 class='blue'>Descripción<h3></td>"
-                    + "</tr>"
-                    + "<tr>"
-                    +  "<td><span>Un grupo de inteligencia militar desea codificar los mensajes secretos de tal forma que no puedan ser interpretados con una lectura directa, para lo cual han establecido las siguientes reglas:<br><br>"
-                    + "<ul>"
-                    +  "<li>El texto cifrado deberá contener sólo letras en mayúscula.</li><br>"
-                    + "<li>Reemplazar cada letra por la que sigue según abecedario, excepto Z que se deberá reemplazar con la letra A.</li><br>"
-                    + "<li>Reemplazar cada dígito encontrado por el siguiente número excepto el 9 que deberá ser reemplazado por el 0.</li><br>"
-                    + "<li>El carácter blanco no se codifica.</li><br>"
-                    + "<li>Una imagen que al hacer click sobre ella, se cambie por otra o se le añada un estilo css diferente.</li>"
-                    + "</ul>"
-                    + "</span></td>"
-                    + "<td>"
-                    + "</tr>"
-                    + "<tr>"
-                    + "<td colspan='3'>"
-                    + "<a class='blue' href='https://pedrop19.github.io/CuentraAtras/'>Ir al proyecto</a>"
-                    + "</td>"
-                    + "</tr>"
-                    + "</table>"
-            } else if (textoIngresado == "cuentaatras") {
-                resultado.textContent = " ";
-                divElement.innerHTML = "<a href='github.com/Pedrop19/'>github.com/Pedrop19/ </a>"
-                    + "<span class='yellow'>" + textoIngresado + "</span><br><br>"
-                    + "<table class='tabla'>"
-                    + "<tr>"
-                    +  "<th colspan='3'><span class='yellow' id='title'>⏳ Cuenta Atrás ⏳<span></th>"
-                    + "</tr>"
-                    + "<tr>"
-                    +  "<td rowspan='3'><img src='./img/cuentaatr.PNG' width='300px'></td>"
-                    +  "<td colspan='3' align='center'><h3 class='blue'>Descripción<h3></td>"
-                    + "</tr>"
-                    + "<tr>"
-                    +  "<td><span>Diseña una página web que muestre una cuenta atrás de los días, minutos y segundos que faltan para tu fecha de cumpleaños. Además, en función de la estación en la que nos encontremos:<br><br>"
-                    + "<ul>"
-                    +  "<li>Otoño: Empieza a finales de Septiembre.</li><br>"
-                    + "<li>Invierno: Empieza a finales de Diciembre.</li><br>"
-                    + "<li>Primavera: Empieza a finales de Marzo.</li><br>"
-                    + "<li>Verano: Empieza a finales de Junio.</li><br>"
-                    + "</ul>"
-                    + "<span>Deberemos mostrar un fondo, imágenes y cualquier elemento extra característico de cada estación. El día de tu cumpleaños, estos elementos deberán ser característicos de un cumpleaños, como la imagen de una tarta, además, el contador deberá ocultarse y mostrar el mensaje: “¡Felicidades!”</span>"
-                    + "</span></td>"
-                    + "<td>"
-                    + "</tr>"
-                    + "<tr>"
-                    + "<td colspan='3'>"
-                    + "<a class='blue' href='https://pedrop19.github.io/CodificadorMensajes/'>Ir al proyecto</a>"
-                    + "</td>"
-                    + "</tr>"
-                    + "</table>"
-            }else if (textoIngresado == "millionarios") {
-                resultado.textContent = " ";
-                divElement.innerHTML = "<a href='github.com/Pedrop19/'>github.com/Pedrop19/ </a>"
-                    + "<span class='yellow'>" + textoIngresado + "</span><br><br>"
-                    + "<table class='tabla'>"
-                    + "<tr>"
-                    +  "<th colspan='3'><span class='yellow' id='title'>💸 Millionarios 💸<span></th>"
-                    + "</tr>"
-                    + "<tr>"
-                    +  "<td rowspan='3'><img src='./img/mill.PNG' width='300px'></td>"
-                    +  "<td colspan='3' align='center'><h3 class='blue'>Descripción<h3></td>"
-                    + "</tr>"
-                    + "<tr>"
-                    +  "<td><span>Inicialmente la aplicación deberá mostrar un usuario como se muestra en la pantalla, se proporciona un método que genera un nombre aleatorio, sólo hay que generar su dinero, que será aleatorio con un máximo de 1.000.000. Diseñar las funcionalidades de los botones que aparecen en pantalla:<br><br>"
-                    + "<ul>"
-                    +  "<li>Añadir usuario: Añade debajo otro usuario aleatorio.</li><br>"
-                    + "<li>Doblar el dinero de cada usuario.</li><br>"
-                    + "<li>Mostrar millonarios, filtrará aquellos usuarios con dinero mayor a 1.000.000 (el resto desaparece).</li><br>"
-                    + "<li>Ordenar por dinero, de mayor a menor.</li><br>"
-                    + "<li>Calcular el dinero total.</li>"
-                    + "</ul>"
-                    + "</span></td>"
-                    + "<td>"
-                    + "</tr>"
-                    + "<tr>"
-                    + "<td colspan='3'>"
-                    + "<a class='blue' href='https://pedrop19.github.io/Millionarios/'>Ir al proyecto</a>"
-                    + "</td>"
-                    + "</tr>"
-                    + "</table>"
-            } else if (textoIngresado == "calendario") {
-                resultado.textContent = " ";
-                divElement.innerHTML = "<a href='github.com/Pedrop19/'>github.com/Pedrop19/ </a>"
-                    + "<span class='yellow'>" + textoIngresado + "</span><br><br>"
-                    + "<table class='tabla'>"
-                    + "<tr>"
-                    +  "<th colspan='3'><span class='yellow' id='title'>📆 Calendario 📆<span></th>"
-                    + "</tr>"
-                    + "<tr>"
-                    +  "<td rowspan='3'><img src='./img/calendario.PNG' width='300px'></td>"
-                    +  "<td colspan='3' align='center'><h3 class='blue'>Descripción<h3></td>"
-                    + "</tr>"
-                    + "<tr>"
-                    +  "<td><span>Crear un calendario con la funcionalidad de poner emojis dependiendo del estado de animo del usuario<br><br>"
-                    + "</span></td>"
-                    + "<td>"
-                    + "</tr>"
-                    + "<tr>"
-                    + "<td colspan='3'>"
-                    + "<a class='blue' href='https://pedrop19.github.io/Estado-de-Animo/'>Ir al proyecto</a>"
-                    + "</td>"
-                    + "</tr>"
-                    + "</table>"
-            } else if (textoIngresado == "cuadro") {
-                resultado.textContent = " ";
-                divElement.innerHTML = "<a href='github.com/Pedrop19/'>github.com/Pedrop19/ </a>"
-                    + "<span class='yellow'>" + textoIngresado + "</span><br><br>"
-                    + "<table class='tabla'>"
-                    + "<tr>"
-                    +  "<th colspan='3'><span class='yellow' id='title'>🔁 Cambio de Color 🔁<span></th>"
-                    + "</tr>"
-                    + "<tr>"
-                    +  "<td rowspan='3'><img src='./img/CambioColor.PNG' width='300px'></td>"
-                    +  "<td colspan='3' align='center'><h3 class='blue'>Descripción<h3></td>"
-                    + "</tr>"
-                    + "<tr>"
-                    +  "<td><span>Cuadrado con multiples divs que cambian de color de forma aleatoria al hacerle hover<br><br>"
-                    + "</span></td>"
-                    + "<td>"
-                    + "</tr>"
-                    + "<tr>"
-                    + "<td colspan='3'>"
-                    + "<a class='blue' href='https://pedrop19.github.io/CambioColor/'>Ir al proyecto</a>"
-                    + "</td>"
-                    + "</tr>"
-                    + "</table>"
-            } else if (textoIngresado == "animacion") {
-                resultado.textContent = " ";
-                divElement.innerHTML = "<a href='github.com/Pedrop19/'>github.com/Pedrop19/ </a>"
-                    + "<span class='yellow'>" + textoIngresado + "</span><br><br>"
-                    + "<table class='tabla'>"
-                    + "<tr>"
-                    +  "<th colspan='3'><span class='yellow' id='title'>🐱‍🏍 Animacion 🐱‍🏍<span></th>"
-                    + "</tr>"
-                    + "<tr>"
-                    +  "<td rowspan='3'><img src='./img/animacion.PNG' width='300px'></td>"
-                    +  "<td colspan='3' align='center'><h3 class='blue'>Descripción<h3></td>"
-                    + "</tr>"
-                    + "<tr>"
-                    +  "<td><span>Animacion simple en la que van cayendo diferentes objetos<br><br>"
-                    + "</span></td>"
-                    + "<td>"
-                    + "</tr>"
-                    + "<tr>"
-                    + "<td colspan='3'>"
-                    + "<a class='blue' href='https://pedrop19.github.io/Animacion/'>Ir al proyecto</a>"
-                    + "</td>"
-                    + "</tr>"
-                    + "</table>"
-            } else {
-                divElement.innerHTML = "<a>github.com/Pedrop19/ </a>"
-                    + "<span class='yellow'>" + textoIngresado + "</span><br><br>"
-                    + "<span class='red'>comando desconocido</span><br>";
-            }
-            textarea.value = "";
-            resultado.append(divElement);
-        }
-    });
-
+            textarea.addEventListener("keydown", function (event) {
+                if (event.key === "Enter") {
+                    let divElement = document.createElement("div");
+                    divElement.className = "linea";
+                    event.preventDefault();
+                    let textoIngresado = textarea.value.toLowerCase();
+                    if (textoIngresado.includes("clear") || textoIngresado.includes("cls")) {
+                        resultado.textContent = " ";
+                    } else if (textoIngresado.includes("help")) {
+                        divElement.innerHTML =
+                            "<a href='github.com/Pedrop19/'>github.com/Pedrop19/</a>" +
+                            "<span class='yellow'>" +
+                            textoIngresado +
+                            "</span><br><br>" +
+                            "<span class='green'>clear o cls</span>: <span class='blue'>Limpia la consola<span><br><br>"
+                            +  "<span class='green'>proyectos</span>: <span class='blue'>Muestra los proyectos disponibles<span>";
+                    } else if (textoIngresado.includes("proyectos")) {
+                        resultado.textContent = " ";
+                        divElement.innerHTML = "<a href='github.com/Pedrop19/'>github.com/Pedrop19/</a>" +
+                            "<span class='yellow'>" + textoIngresado + "</span><br><br>" +
+                            "<span class='blue'>💻 Proyectos(escribe el comando para acceder al proyecto deseado):</span><br><br>" +
+                            "<span class='green'>codificador</span><br><br>" +
+                            "<span class='green'>cuentaatras</span><br><br>" +
+                            "<span class='green'>millionarios</span><br><br>" +
+                            "<span class='green'>calendario</span><br><br>" +
+                            "<span class='green'>cuadro</span><br><br>" +
+                            "<span class='green'>animacion</span><br><br>";
+                    } else if (proyectos.hasOwnProperty(textoIngresado)) {
+                        resultado.textContent = " ";
+                        const proyecto = proyectos[textoIngresado];
+                        divElement.innerHTML = `
+                            <a href="${proyecto.github_link}">${proyecto.github_link}</a>
+                            <span class="yellow">${textoIngresado}</span><br><br>
+                            <table class="tabla">
+                            <tr>
+                                <th colspan="3"><span class="yellow" id="title">${proyecto.title}</span></th>
+                            </tr>
+                            <tr>
+                                <td rowspan="3"><img src="${proyecto.image_url}" width="300px"></td>
+                                <td colspan="3" align="center"><h3 class="blue">Descripción</h3></td>
+                            </tr>
+                            <tr>
+                                <td><span>${proyecto.description}</span></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td colspan="3">
+                                <a class="blue" href="${proyecto.project_link}">Ir al proyecto</a>
+                                </td>
+                            </tr>
+                            </table>
+                        `;
+                    } else {
+                        divElement.innerHTML =
+                            "<a href='github.com/Pedrop19/'>github.com/Pedrop19/</a>" +
+                            "<span class='yellow'>" +
+                            textoIngresado +
+                            "</span><br><br>" +
+                            "<span class='red'>comando desconocido</span><br>";
+                    }
+                    
+                    textarea.value = "";
+                    resultado.append(divElement);
+                    console.log(textoIngresado);
+                }
+            });
+        });
 });
