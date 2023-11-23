65ea92f5eb5b44277652f6f50f3d9f2f22ec7bed
Pedrop19 <prlazaron01@iesalbarregas.es>
2023-11-23 10:54:29 +0100

    Add json-server dependency to package.json

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     divElement.className = "linea";
                     event.preventDefault();
                     let textoIngresado = textarea.value.toLowerCase();
+
                     if (textoIngresado.includes("clear") || textoIngresado.includes("cls")) {
                         resultado.textContent = " ";
                     } else if (textoIngresado.includes("help")) {
@@ -23,62 +24,64 @@ document.addEventListener("DOMContentLoaded", function () {
                             "</span><br><br>" +
                             "<span class='green'>clear o cls</span>: <span class='blue'>Limpia la consola<span><br><br>"
                             + "<span class='green'>js</span>: <span class='blue'>Muestra los proyectos en JavaScript<span>"
-                            + "<span class='green'>java</span>: <span class='blue'>Muestra los proyectos en Java<span>";;
+                            + "<span class='green'>java</span>: <span class='blue'>Muestra los proyectos en Java<span>";
                     } else if (textoIngresado.includes("js")) {
                         resultado.textContent = " ";
                         divElement.innerHTML = "<a href='github.com/Pedrop19/'>github.com/Pedrop19/</a>" +
                             "<span class='yellow'>" + textoIngresado + "</span><br><br>" +
-                            "<span class='blue'>💻 Proyectos(escribe el comando para acceder al proyecto deseado):</span><br><br>" +
-                            "<span class='green'>codificador</span><br><br>" +
-                            "<span class='green'>cuentaatras</span><br><br>" +
-                            "<span class='green'>millionarios</span><br><br>" +
-                            "<span class='green'>calendario</span><br><br>" +
-                            "<span class='green'>cuadro</span><br><br>" +
-                            "<span class='green'>animacion</span><br><br>" +
-                            "<span class='green'>blanconegro</span><br><br>" +
-                            "<span class='green'>asteroids</span><br><br>" +
-                            "<span class='green'>millionarios2</span><br><br>" +
-                            "<span class='green'>notas</span><br><br>" +
-                            "<span class='green'>pokemon</span><br><br>" +
-                            "<span class='green'>lista-pokemon</span><br><br>";
+                            "<span class='blue'>💻 Proyectos(escribe el comando para acceder al proyecto deseado):</span><br><br>";
+                        Object.keys(proyectos.js).forEach(proyectoKey => {
+                            divElement.innerHTML += `<span class='green'>${proyectoKey}</span><br><br>`;
+                        });
                     } else if (textoIngresado.includes("java")) {
                         resultado.textContent = " ";
                         divElement.innerHTML = "<a href='github.com/Pedrop19/'>github.com/Pedrop19/</a>" +
                             "<span class='yellow'>" + textoIngresado + "</span><br><br>" +
-                            "<span class='blue'>💻 Proyectos(escribe el comando para acceder al proyecto deseado):</span><br><br>" +
-                            `${proyectos.map(proyecto => `<span class='green'>${proyecto.name}</span><br><br>`).join('')}`;
-                    } else if (proyectos.hasOwnProperty(textoIngresado)) {
-                        resultado.textContent = " ";
-                        const proyecto = proyectos[textoIngresado];
-                        divElement.innerHTML = `
-                            <a href="${proyecto.github_link}">${proyecto.github_link}</a>
-                            <span class="yellow">${textoIngresado}</span><br><br>
-                            <table class="tabla">
-                            <tr>
-                                <th colspan="3"><span class="yellow" id="title">${proyecto.title}</span></th>
-                            </tr>
-                            <tr>
-                                <td rowspan="3"><img src="${proyecto.image_url}" width="300px"></td>
-                                <td colspan="3" align="center"><h3 class="blue">Descripción</h3></td>
-                            </tr>
-                            <tr>
-                                <td><span>${proyecto.description}</span></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td colspan="3">
-                                <a class="blue" href="${proyecto.project_link}">Ir al proyecto</a>
-                                </td>
-                            </tr>
-                            </table>
-                        `;
+                            "<span class='blue'>💻 Proyectos(escribe el comando para acceder al proyecto deseado):</span><br><br>";
+                        Object.keys(proyectos.java).forEach(proyectoKey => {
+                            divElement.innerHTML += `<span class='green'>${proyectoKey}</span><br><br>`;
+                        });
                     } else {
-                        divElement.innerHTML =
-                            "<a href='github.com/Pedrop19/'>github.com/Pedrop19/</a>" +
-                            "<span class='yellow'>" +
-                            textoIngresado +
-                            "</span><br><br>" +
-                            "<span class='red'>comando desconocido</span><br>";
+                        let proyectoEncontrado = null;
+
+                        if (proyectos.js.hasOwnProperty(textoIngresado)) {
+                            proyectoEncontrado = proyectos.js[textoIngresado];
+                        } else if (proyectos.java.hasOwnProperty(textoIngresado)) {
+                            proyectoEncontrado = proyectos.java[textoIngresado];
+                        }
+
+                        if (proyectoEncontrado) {
+                            resultado.textContent = " ";
+                            divElement.innerHTML = `
+                                <a href="${proyectoEncontrado.github_link}">${proyectoEncontrado.github_link}</a>
+                                <span class="yellow">${textoIngresado}</span><br><br>
+                                <table class="tabla">
+                                <tr>
+                                    <th colspan="3"><span class="yellow" id="title">${proyectoEncontrado.title}</span></th>
+                                </tr>
+                                <tr>
+                                    <td rowspan="3"><img src="${proyectoEncontrado.image_url}" width="300px"></td>
+                                    <td colspan="3" align="center"><h3 class="blue">Descripción</h3></td>
+                                </tr>
+                                <tr>
+                                    <td><span>${proyectoEncontrado.description}</span></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td colspan="3">
+                                        <a class="blue" href="${proyectoEncontrado.project_link}">Ir al proyecto</a>
+                                    </td>
+                                </tr>
+                                </table>
+                            `;
+                        } else {
+                            divElement.innerHTML =
+                                "<a href='github.com/Pedrop19/'>github.com/Pedrop19/</a>" +
+                                "<span class='yellow'>" +
+                                textoIngresado +
+                                "</span><br><br>" +
+                                "<span class='red'>comando desconocido</span><br>";
+                        }
                     }
 
                     textarea.value = "";
